f0144fe17777ee02a90fe4d5eaa419cd32d3abb6
James Lenahan <james.k.lenahan@gmail.com>
2019-11-30 15:45:39 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,7 +3,7 @@ array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
 def my_collect(array)
   i = 0 
   collection = []
-  while i 
-  array.split("").first
+  while i < array.length 
+    yield 
 end
   
